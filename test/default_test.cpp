941cf6875b413e3f13af6d96f0f794b002b5965c
David Guevara <37723586+DatWaft@users.noreply.github.com>
2019-09-08 14:07:54 -0600

    Creado el archivo de testeo básico.

---
+++ test/default_test.cpp
@@ -0,0 +1,12 @@
+// default_test.cpp
+// Autores: David Guevara & Mario Arguello
+// Descripción: Archivo de pruebas principal
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  cout << "Hola mundo" << endl;
+}
