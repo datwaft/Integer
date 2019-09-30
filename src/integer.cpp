1be52d171d7b831123e9179dc7e4087493b6c73a
David Guevara <37723586+DatWaft@users.noreply.github.com>
2019-09-29 19:58:42 -0600

    Update integer.cpp

--- src/integer.cpp
+++ src/integer.cpp
@@ -256,7 +256,7 @@ int Integer::getCurrentSize() const
 
 int Integer::getDigits() const
 {
-  return static_cast<int>(this->toString().size());
+  return static_cast<int>(this->toString().size()) - (this->sign_ == NEGATIVE ? 1 : 0);
 }
 
 bool Integer::operator==(const Integer& other) const
@@ -629,17 +629,16 @@ Integer Integer::operator*(const Integer& other) const
 
 Integer Integer::operator/(const Integer& other) const
 {
-  std::string dividend = this->toString();
-  std::string divisor = other.toString();
-  long long result;
-  bool flag = false;
+  if (other == 0)
+    throw std::exception("Division by 0");
 
-  /* if (dividend.size() <= 18)
-     if (divisor.size() <= 18)
-     {
-       result  = (std::stoll(dividend) / std::stoll(divisor));
-       return std::to_string(result);
-     }*/
+  std::string dividend = this->toString();
+  if (this->sign_ == NEGATIVE)
+    dividend = dividend.substr(1);
+  std::string divisor = other.toString();
+  if (other.sign_ == NEGATIVE)
+    divisor = divisor.substr(1);
+  bool flag = false;
 
   std::string timesin = "0";
   std::string counter = "0";
@@ -689,8 +688,15 @@ Integer Integer::operator/(const Integer& other) const
   }
   if (auxdividend.size() > 0 && checkForCero(auxdividend))
     resultstring = resultstring + "0";
-  return resultstring;
 
+  Integer result_aux(resultstring);
+
+  if (this->sign_ != NEGATIVE && other.sign_ == NEGATIVE)
+    result_aux.sign_ = NEGATIVE;
+  else if (this->sign_ == NEGATIVE && other.sign_ != NEGATIVE)
+    result_aux.sign_ = NEGATIVE;
+
+  return result_aux;
 }
 
 std::string Integer::toString() const
@@ -841,7 +847,7 @@ void Integer::Split(Integer* high, Integer* low, int pivot) const
 
 Integer Integer::AddRightPadding(int padding) const
 {
-  return (this->sign_ == NEGATIVE ? "-" : "") + this->toString() + std::string(padding, '0');
+  return this->toString() + std::string(padding, '0');
 }
 
 void Integer::Clear()
